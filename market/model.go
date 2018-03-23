b34ecb1f8635da989f063b5ce39768d5c917da83
Morten Klim Sørensen <mks@one.com>
2018-03-23 16:51:10 +0100

    Adds KLine endpoint

--- market/model.go
+++ market/model.go
@@ -1,5 +1,10 @@
 package market
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Params struct {
 	Symbol string `json:"symbol"`
 }
@@ -45,4 +50,39 @@ type AllBookTickers []struct {
 	BidQty   string `json:"bidQty"`
 	AskPrice string `json:"askPrice"`
 	AskQty   string `json:"askQty"`
+}
+
+type KLine struct {
+	OpenTime              int64
+	Open                  string
+	High                  string
+	Low                   string
+	Close                 string
+	Volume                string
+	CloseTime             int64
+	QuoteAssetVol         string
+	NoTrades              int
+	TakerBuyBaseAssetVol  string
+	TakerBuyQuoteAssetVol string
+	Ignore                string
+}
+
+type KLineParams struct {
+	Symbol    string `json:"symbol"`
+	Interval  string `json:"interval"`        //Possible values: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+	Limit     int    `json:"limit,omitempty"` //Default 500; max 500.
+	StartTime int64  `json:"startTime,omitempty"`
+	EndTime   int64  `json:"endTime,omitempty"`
+}
+
+func (k *KLine) UnmarshalJSON(buf []byte) error {
+	tmp := []interface{}{&k.OpenTime, &k.Open, &k.High, &k.Low, &k.Close, &k.Volume, &k.CloseTime, &k.QuoteAssetVol, &k.NoTrades, &k.TakerBuyBaseAssetVol, &k.TakerBuyQuoteAssetVol, &k.Ignore}
+	wantLen := len(tmp)
+	if err := json.Unmarshal(buf, &tmp); err != nil {
+		return err
+	}
+	if g, e := len(tmp), wantLen; g != e {
+		return fmt.Errorf("wrong number of fields in KLine: %d != %d", g, e)
+	}
+	return nil
 }
